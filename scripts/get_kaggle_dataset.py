5f16873dc09650c18d992931a0597b05059e720a
Keith Thomson <camelbyte@proton.me>
2025-11-25 10:52:39 -0500

    Added a notebooks directory for centralized notebook repository.

--- scripts/get_kaggle_dataset.py
+++ scripts/get_kaggle_dataset.py
@@ -9,16 +9,13 @@ def download_kaggle_dataset(dataset_name, download_path):
     dataset_name (str): The name of the dataset in the format 'username/dataset-name'.
     download_path (str): The local path where the dataset should be downloaded.
     """
-    # Ensure the download path exists
     os.makedirs(download_path, exist_ok=True)
     
-    # Download the dataset
     kaggle.api.dataset_download_files(dataset_name, path=download_path, unzip=True)
     print(f"Dataset '{dataset_name}' downloaded to '{download_path}'")
 
 
 if __name__ == "__main__":
-    # Example usage
-    dataset_name = "zynicide/wine-reviews"  # Replace with your desired dataset
-    download_path = "./data/wine-reviews"   # Replace with your desired download path
+    dataset_name = "zynicide/wine-reviews"  # Replace 
+    download_path = "./data/wine-reviews"   # Replace 
     download_kaggle_dataset(dataset_name, download_path)
